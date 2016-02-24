c3d94bde88f97a866c8949ba85ece4678084aa63
Michael Ummels <michael@ummels.de>
2016-02-24 03:26:26 +0100

    Simplify test.js

--- test.js
+++ test.js
@@ -1,24 +1,5 @@
-var index = require('./index.js');
+var Comments = require('./comments.js');
+var config = require('./config.json');
 var event = require('./test_event.json');
-
-var context = {
-  succeed: function(result) {
-    console.log('Success with result:', JSON.stringify(result, null, 2));
-    process.exit(0);
-  },
-  fail: function(err) {
-    console.log('Failure with error:', err.toString());
-    process.exit(1);
-  },
-  done: function(error, result) {
-    if (error != null)
-      this.fail(error);
-    else
-      this.succeed(result);
-  },
-  getRemainingTimeInMillis: function() {
-    return Number.POSITIVE_INFINITY;
-  }
-}
-
-index.handler(event, context);
+var comments = new Comments(config);
+comments.submit(event)
