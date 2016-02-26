76a50e86276a03648229932d5a0ff8df49942549
Michael Ummels <michael@ummels.de>
2016-02-26 23:47:18 +0100

    Make test output result

--- test.js
+++ test.js
@@ -2,4 +2,5 @@ var Comments = require('./comments.js');
 var config = require('./config.json');
 var event = require('./test_event.json');
 var comments = new Comments(config);
-comments.submit(event)
+var url = comments.submit(event)
+console.log(url);
