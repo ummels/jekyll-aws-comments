4b3e31d30c9fd8bc113fffad410db31427e58105
Michael Ummels <michael@ummels.de>
2016-03-05 17:24:37 +0100

    Make test output result of promise

--- test.js
+++ test.js
@@ -2,5 +2,11 @@ var Comments = require('./comments.js');
 var config = require('./config.json');
 var event = require('./test_event.json');
 var comments = new Comments(config);
-var url = comments.submit(event)
-console.log(url);
+
+comments.submit(event)
+.then(function(url) {
+  console.log(url);
+})
+.catch(function(err) {
+  console.log(err);
+});
