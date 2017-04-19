171624227828c69015701f908923802a16a296f0
Michael Ummels <michael@ummels.de>
2017-04-19 23:19:44 +0200

    Use callback instead of context

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var Comments = require('./comments.js');
 var config = require('./config.json');
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context, callback) {
   try {
     var comments = new Comments(config);
     comments.submit(event)
     .then(function(url) {
-      context.succeed(url);
+      callback(null, url);
     })
     .catch(function(err) {
-      context.fail('Server error');
+      callback(err);
     });
   } catch(err) {
-    context.fail('Bad request');
+    callback(err);
   }
 }
